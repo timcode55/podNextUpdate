4ad38eeec4a41bfff262eab6c92777fad1a35210
timcode55 <extramailboxes@gmail.com>
2023-05-23 12:51:12 -0700

    Fix background issue

--- components/podCard.js
+++ components/podCard.js
@@ -10,49 +10,53 @@ const PodCard = (props) => {
   return (
     <div className={classes.divStyle}>
       <div className={classes.podcontainer}>
-        <a href={podcast.listennotes_url} target="_blank" rel="noreferrer">
-          <img className={classes.podimage} src={podcast.image} alt="pod1" />
-        </a>
-        <div className={classes.podtitle}>
-          <h1>{podcast.title.substring(0, 52)}</h1>
-        </div>
-        <div className={classes.desc}>
-          <p className={classes.ptext}>
-            {podcast.description.substring(0, 200).replace(/(<([^>]+)>)/gi, "")}
-            ...
-          </p>
-        </div>
-        <div className={classes.podButtons}>
-          {/* <div className={classes.webButton}> */}
-          <a href={podcast.website} target="_blank" rel="noreferrer">
-            <button className={classes.webButton}>Website</button>
+        <div className={classes.podcontent}>
+          <a href={podcast.listennotes_url} target="_blank" rel="noreferrer">
+            <img className={classes.podimage} src={podcast.image} alt="pod1" />
           </a>
-          {/* </div> */}
-          {/* <div className={classes.webButton}> */}
-          <a href={podcast.itunes} target="_blank" rel="noreferrer">
-            <button className={classes.webButton}>iTunes Link</button>
-          </a>
-          {/* </div> */}
-        </div>
+          <div className={classes.podtitle}>
+            <h1>{podcast.title.substring(0, 52)}</h1>
+          </div>
+          <div className={classes.desc}>
+            <p className={classes.ptext}>
+              {podcast.description
+                .substring(0, 200)
+                .replace(/(<([^>]+)>)/gi, "")}
+              ...
+            </p>
+          </div>
+          <div className={classes.podButtons}>
+            {/* <div className={classes.webButton}> */}
+            <a href={podcast.website} target="_blank" rel="noreferrer">
+              <button className={classes.webButton}>Website</button>
+            </a>
+            {/* </div> */}
+            {/* <div className={classes.webButton}> */}
+            <a href={podcast.itunes} target="_blank" rel="noreferrer">
+              <button className={classes.webButton}>iTunes Link</button>
+            </a>
+            {/* </div> */}
+          </div>
 
-        <div className={classes.contratings}>
-          <div>
-            <Image
+          <div className={classes.contratings}>
+            <div>
+              {/* <Image
               className={classes.ratingimage}
               src={icons8}
               alt="ratingimage"
-            />
-            <p className={classes.ratingtext}># of Ratings</p>
-            <p className={classes.ratingtext}>{podcast.numberOfRatings}</p>
-          </div>
-          <div>
-            <Image
+            /> */}
+              <p className={classes.ratingtext}># of Ratings</p>
+              <p className={classes.ratingtext}>{podcast.numberOfRatings}</p>
+            </div>
+            <div>
+              {/* <Image
               className={classes.ratingimage}
               src={rating}
               alt="ratingimage"
-            />
-            <p className={classes.ratingtext}>iTunes Rating</p>
-            <p className={classes.ratingtext}>{podcast.rating}</p>
+            /> */}
+              <p className={classes.ratingtext}>iTunes Rating</p>
+              <p className={classes.ratingtext}>{podcast.rating}</p>
+            </div>
           </div>
         </div>
       </div>
