6e8358a19132c32a4bc8dcd19a0bbc1fba663a0d
timcode55 <extramailboxes@gmail.com>
2023-06-02 09:59:08 -0500

    Fix analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,34 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <script
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-T4C827WN5N"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-T4C827WN5N', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+
+      {/* <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-T4C827WN5N"
-      ></script>
-      <script>
+      ></Script>
+      <Script>
         window.dataLayer = window.dataLayer || []; function gtag()
         {dataLayer.push(arguments)}
         gtag('js', new Date()); gtag('config', 'G-T4C827WN5N');
-      </script>
+      </Script> */}
       <PodcastContextProvider>
         <Head>
           <meta
