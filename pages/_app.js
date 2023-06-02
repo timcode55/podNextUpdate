500fad81bc0fa92f598fce7ad94457f0d70e8039
timcode55 <extramailboxes@gmail.com>
2023-06-02 09:48:48 -0500

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,31 @@
 import { PodcastContextProvider } from "../store/podcastContext";
 import "../styles/globals.css";
+import Script from "next/script";
 
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PodcastContextProvider>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </PodcastContextProvider>
+    <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-T4C827WN5N"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-T4C827WN5N');
+      </script>
+      <PodcastContextProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </PodcastContextProvider>
+    </>
   );
 }
 
